cbe6898e90946c173acee849e781379841b2dcf2
josealt2197 <josealt2197@gmail.com>
2020-10-28 14:38:34 -0600

    Procedimientos para Menus y structs para manejo de datos

--- solucion_computacional/PP1_AED.c
+++ solucion_computacional/PP1_AED.c
@@ -1,17 +1,316 @@
-# Algoritmos y Estructuras de Datos - Grupo 1
-# Proyecto Programado 1 
-# Estudiantes: Jose Manuel Altamirano Salazar - 2020426159
-#              Josué Brenes Alfaro - 2020054427
+/*Algoritmos y Estructuras de Datos - Grupo 1
+	Proyecto Programado 1 
+	Estudiantes: Jose Altamirano Salazar - 2020426159
+	             Josué Brenes Alfaro - 2020054427
+*/
   
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+#include<time.h>
 
-int main (void){
+//Procedimientos para Menus de Opciones
+void MenuPrincipal();
+void GestionEquipo();
+void GestionAsignacion();
+void GestionRequerimiento();
+void GestionIncidentes();
+void AnalisisDeDatos();
+
+struct Requerimiento{
+    char identificador[50];
+    char tipo[12];
+    char descripcion[100];
+    char riesgo[60];
+    char dependencia[100];
+    char recursos[55];
+    char estado [15];
+    char esfuerzo[5];
+}datosRequerimiento;
+
+struct MiembroEquipo{
+    char nombre_completo[50];
+    char cedula[12];
+    char correo[50];
+    char nivel_acceso[3];
+    char telefono[15];
+}datosMiembroEquipo;
+
+struct Asignacion{
+    char fechaSolicitud[50];
+    char horaInicio[15];
+    char horaFin[15];
+    char recurso[50];
+    char Identificador[10];
+    char descripcion[100];
+    char miembros[15];
+    char prioridad[15];
+    char estado[15];
+}datosAsignacion;
+
+struct Incidentes{
+    char codigoRequerimiento[15];
+    char codigoAsignacion[15];
+    char descripcionIncidente[100];
+    char fecha[20];
+}datosIncidentes;
+
+void Temporal(){
+	getchar();
+}
+
+void MenuPrincipal(){
+	char opcion;
+	
+	do{
+        system( "CLS" );
+        fflush(stdin);
+        printf("\n\n+-------------------------------+\n");
+		printf("      Gestor de Requerimientos          \n");
+		printf("+-------------------------------+\n");
+		printf( "\t  Menu Principal\n" );
+		printf("+-------------------------------+\n");
+		printf( "\n 1 - Gestion de Miembros de Equipo" );
+	    printf( "\n 2 - Gestion de Requerimientos" );
+	    printf( "\n 3 - Gestion de Asignaciones" );
+	    printf( "\n 4 - Gestion de Incidencias" );
+		printf( "\n 5 - Analisis de Datos" );   
+	    printf( "\n 6 - Salir" );
+	    printf("\n");
+	    printf("\n\n Elija una opcion : ");
+        opcion = getchar();
+        
+        switch ( opcion ){
+            case '1': GestionEquipo();
+                break;
+			case '2': GestionRequerimiento();
+                break;
+            case '3': GestionAsignacion();
+                break;
+			case '4': GestionIncidentes();
+                break;
+            case '5': AnalisisDeDatos();
+				break;
+			case '6': exit(1);
+				break;
+            default:
+            	fflush(stdin);
+				printf("**Opcion no valida. Intente de nuevo.**\n");
+				getchar();
+                break;
+        }
+    } while (opcion!='6');    
+    fflush(stdin);
+	getchar();	
+}
+
+void GestionEquipo(){
+	char opcion, ch;	
+
+	do{
+		system( "CLS" );
+		fflush(stdin);
+		printf("\n\n+---------------------------------+\n");
+		printf("      Gestor de Requerimientos          \n");
+		printf("+---------------------------------+\n");
+		printf( "   Gestion de Miembros de Equipo\n" );
+		printf("+---------------------------------+\n");
+		printf("\n 0. Volver al Menu Principal.");
+		printf("\n 1. Registrar un Miembro de Equipo.");
+		printf("\n 2. Consultar un Miembro de Equipo.");
+		printf("\n");
+		printf("\n\nElija una opcion : ");	
+		opcion=getchar();
 		
+		while((ch = getchar()) != EOF && ch != '\n');
+			switch(opcion){
+				case '1': Temporal();
+					break;
+				case '2': Temporal();
+					break;
+				case '0': MenuPrincipal();
+					break;
+				default:
+					fflush(stdin);
+					printf("**Opcion no valida. Intente de nuevo.**\n");
+					getchar();
+					break;	
+		}	
+	}while(opcion!=3);			
+	fflush(stdin);
+	getchar();
+}
 
+void GestionRequerimiento(){
+	char opcion, ch;	
 
-	return 0;
+	do{
+		system( "CLS" );
+		fflush(stdin);
+        printf("\n\n+-------------------------------+\n");
+		printf("      Gestor de Requerimientos          \n");
+		printf("+-------------------------------+\n");
+		printf( "   Gestion de Requerimientos\n" );
+		printf("+-------------------------------+\n");
+		printf("\n 0. Volver al Menu Principal.");
+		printf("\n 1. Agregar nuevo requerimiento.");
+		printf("\n 2. Modificar un requerimiento.");
+		printf("\n 3. Calificar un requerimiento.");
+		printf("\n 4. Consultar un requerimiento.");
+		printf("\n");
+		printf("\n\nElija una opcion : ");	
+		opcion=getchar();
 		
+		while((ch = getchar()) != EOF && ch != '\n');
+			switch(opcion){
+				case '1': Temporal();
+					break;
+				case '2': Temporal();
+					break;
+				case '3': Temporal();
+					break;
+				case '4': Temporal();
+					break;
+				case '0': MenuPrincipal();
+					break;
+				default:
+					fflush(stdin);
+					printf("**Opcion no valida. Intente de nuevo.**\n");
+					getchar();
+					break;	
+		}	
+	}while(opcion!=3);			
+	fflush(stdin);
+	getchar();
+}
+
+void GestionAsignacion(){
+	char opcion, ch;	
+
+	do{
+		system( "CLS" );
+		fflush(stdin);
+        printf("\n\n+-------------------------------+\n");
+		printf("      Gestor de Requerimientos          \n");
+		printf("+-------------------------------+\n");
+		printf( "     Gestion de Asignaciones\n" );
+		printf("+-------------------------------+\n");
+		printf("\n 0. Volver al Menu Principal.");
+		printf("\n 1. Crear una nueva asignación.");
+		printf("\n 2. Consultar asignaciones de un miembro.");
+		printf("\n 3. Cancelar una asignacion.");
+		printf("\n 4. Atender una asignacion.");
+		printf("\n");
+		printf("\n\nElija una opcion : ");	
+		opcion=getchar();
+		
+		while((ch = getchar()) != EOF && ch != '\n');
+			switch(opcion){
+				case '1': Temporal();
+					break;
+				case '2': Temporal();
+					break;
+				case '3': Temporal();
+					break;
+				case '4': Temporal();
+					break;
+				case '0': MenuPrincipal();
+					break;
+				default:
+					fflush(stdin);
+					printf("**Opcion no valida. Intente de nuevo.**\n");
+					getchar();
+					break;	
+		}	
+	}while(opcion!=3);			
+	fflush(stdin);
+	getchar();
+}
+
+void GestionIncidentes(){
+	char opcion, ch;	
+
+	do{
+		system( "CLS" );
+		fflush(stdin);
+        printf("\n\n+-------------------------------+\n");
+		printf("      Gestor de Requerimientos          \n");
+		printf("+-------------------------------+\n");
+		printf( "      Gestion de Incidentes\n" );
+		printf("+-------------------------------+\n");
+		printf("\n 0. Volver al Menu Principal.");
+		printf("\n 1. Registrar un incidente.");
+		printf("\n 2. Consultar por rango de fechas.");
+		printf("\n 3. Consultar por codigo de requerimiento.");
+		printf("\n");
+		printf("\n\nElija una opcion : ");	
+		opcion=getchar();
+		
+		while((ch = getchar()) != EOF && ch != '\n');
+			switch(opcion){
+				case '1': Temporal();
+					break;
+				case '2': Temporal();
+					break;
+				case '0': MenuPrincipal();
+					break;
+				default:
+					fflush(stdin);
+					printf("**Opcion no valida. Intente de nuevo.**\n");
+					getchar();
+					break;	
+		}	
+	}while(opcion!=3);			
+	fflush(stdin);
+	getchar();
+}
+
+void AnalisisDeDatos(){
+	char opcion, ch;	
+
+	do{
+		system( "CLS" );
+		fflush(stdin);
+        printf("\n\n+-------------------------------+\n");
+		printf("      Gestor de Requerimientos          \n");
+		printf("+-------------------------------+\n");
+		printf( "\t  Analisis de Datos\n" );
+		printf("+-------------------------------+\n");
+		printf("\n 0. Volver al Menu Principal.");
+		printf("\n 1. Mostrar requerimientos con más asignaciones.");
+		printf("\n 2. Mostrar horarios más utilizados.");
+		printf("\n 3. Mostrar personas que son mas asignadas.");
+		printf("\n 4. Mostrar requerimientos con mayor esfuerzo.");
+		printf("\n");
+		printf("\n\nElija una opcion : ");	
+		opcion=getchar();
+		
+		while((ch = getchar()) != EOF && ch != '\n');
+			switch(opcion){
+				case '1': Temporal();
+					break;
+				case '2': Temporal();
+					break;
+				case '3': Temporal();
+					break;
+				case '4': Temporal();
+					break;
+				case '0': MenuPrincipal();
+					break;
+				default:
+					fflush(stdin);
+					printf("**Opcion no valida. Intente de nuevo.**\n");
+					getchar();
+					break;	
+		}	
+	}while(opcion!=3);			
+	fflush(stdin);
+	getchar();
+}
+
+
+int main(){   
+    MenuPrincipal();    
+	return 0; 
 }
