c02b34e79322a72cb8432e76d1434e0c07a29cd2
josealt2197 <josealt2197@gmail.com>
2020-11-02 21:51:50 -0600

    Tarea 5 - Algoritmo de Busqueda Lineal y Binaria

--- solucion_computacional/Tarea_5.c
+++ solucion_computacional/Tarea_5.c
@@ -4,73 +4,228 @@
 	             Josué Brenes Alfaro - 2020054427
 */
 
-#include<stdio.h>  
+#include<stdio.h> 
+#include <stdlib.h>
 
-void linearSearch()  
-{  
-    int a[10] = {10, 23, 40, 1, 2, 0, 14, 13, 50, 9};  
-    int item, i,flag;  
-    printf("\nEnter Item which is to be searched\n");  
-    scanf("%d",&item);  
-    for (i = 0; i< 10; i++)  
-    {  
-        if(a[i] == item)   
-        {  
-            flag = i+1;  
-            break;  
-        }   
-        else   
-        flag = 0;  
-    }   
-    if(flag != 0)  
-    {  
-        printf("\nItem found at location %d\n",flag);  
-    }  
-    else  
-    {  
-        printf("\nItem not found\n");   
-    }  
-}   
+#define CANTIDAD 500
 
-int binarySearch(int a[], int beg, int end, int item)  
+typedef struct Lista Lista;
+typedef struct Nodo Nodo;
+
+struct Nodo
+{
+	int dato;
+	Nodo *siguiente;
+};
+
+struct Lista
+{
+	int tamano;
+	Nodo *inicio;
+};
+
+Lista *listaNueva(void)
+{
+	Lista *L;
+	L = (Lista *) malloc(sizeof(Lista));
+	L->inicio = NULL;
+	L->tamano=0;
+	return L;
+}
+
+//Insertar el primer dato o un dato al final de la lista creada
+void insertarDato(Lista *L, int dato)
+{
+	Nodo *n, *aux;
+	L->tamano=L->tamano+1;
+	
+	if(L->inicio == NULL) 
+	{
+		//Inserta el primer nodo de la lista
+		L->inicio = (Nodo *) malloc(sizeof(Nodo));
+		L->inicio->dato = dato;
+		L->inicio->siguiente = NULL;
+		
+		return;
+	}
+	n = L->inicio;
+	while(n!= NULL)
+	{
+		aux = n;
+		n = n->siguiente;		
+	}
+	//Inserta al final de la lista
+	aux->siguiente = (Nodo *) malloc(sizeof(Nodo));
+	aux->siguiente->siguiente = NULL;
+	aux->siguiente->dato = dato;
+
+}
+
+//Mostrar los valores contenids en los nodos de la lista
+void mostrarLista(const Lista *L)
+{
+	Nodo *i;
+	printf("-->La lista es: ");
+	for(i = L->inicio; i!= NULL; i = i->siguiente)
+		printf("[%d]->", i->dato);
+	printf("|\n");
+}
+
+//Retorna el tamano de la lista
+int obtenerTamano(const Lista *L)
+{
+	return L->tamano;
+}
+
+//Liberar el espacio en memoria ocupado por los nodos de la lista creada
+void liberarLista(Lista *L)
+{
+	Nodo *n, *aux;
+	if(L->inicio == NULL)
+		return;
+	if(L->inicio->siguiente == NULL)
+		return;		
+	n = L->inicio;
+	
+	while(n != NULL)
+	{
+		aux = n;
+		n = n->siguiente;
+		free(aux);
+	}
+	
+} 
+
+//Buscar un dato dentro de la lista generada con el algoritmo de busqueda lineal
+int findLinealSearch(Lista *L, int dato){
+	
+	Nodo *nAux = L->inicio;
+	int posicion = 0;
+	
+	while(nAux != NULL)//Se recorre la lista del primer al ultimo valor
+	{
+		if(nAux->dato == dato)//Si el dato de nodo al que apunta nAux coincide con el recibido
+			return posicion+1; //se retorna la posición del nodo nAux en la lista
+		nAux = nAux->siguiente;	
+		posicion = posicion + 1;	
+	}
+	return -1;//Si no se halla el valor, se retorna -1
+	
+}
+  
+//Buscar un dato dentro de la lista generada con el algoritmo de busqueda binaria
+int findBinarySearch(Lista *L, int dato, int inicio, int fin)  
 {  
-    int mid;  
-    if(end >= beg)   
-    {     
-        mid = (beg + end)/2;  
-        if(a[mid] == item)  
+    int centro, valorCentral, posicion=0;
+    Nodo *nAux = L->inicio;
+
+	if(L->inicio->dato == dato){
+		return 1;
+	}
+    
+    if(fin >= inicio){ 
+	    
+        centro = (inicio + fin)/2;
+        
+		while(posicion!=centro)
+		{
+			valorCentral = nAux->dato; 
+			nAux = nAux->siguiente;	
+			posicion = posicion + 1;	
+		}
+				  
+        if(valorCentral == dato)  
         {  
-            return mid+1;  
+            return centro;  
         }  
-        else if(a[mid] < item)   
+        else if(valorCentral < dato)   
         {  
-            return binarySearch(a,mid+1,end,item);  
+            return findBinarySearch(L, dato, centro+1, fin);  
         }  
         else   
         {  
-            return binarySearch(a,beg,mid-1,item);  
+            return findBinarySearch(L, dato, inicio, centro-1);  
         }  
       
     }  
     return -1;   
 }  
 
+//Generar un numero aleatorio
+int numeroAleatorio(){
+    int numero;
+
+    numero = (rand() % (CANTIDAD - 1 + 1)) + 1; 
+
+    return numero;
+}
+
+
+//Invocar a las funciones creadas
 void main ()  
 {  
-    int arr[10] = {16, 19, 20, 23, 45, 56, 78, 90, 96, 100};  
-    int item, location=-1;   
-    printf("Enter the item which you want to search ");  
-    scanf("%d",&item);  
-    location = binarySearch(arr, 0, 9, item);  
-    if(location != -1)   
-    {  
-        printf("Item found at location %d",location);  
-    }  
-    else  
-    {  
-        printf("Item not found");  
-    }  
-} 
+	Lista *L, *L2;
+	int resultado, aleatorio, i;
+	
+	L = listaNueva();
+	L2 = listaNueva();
+	
+	srand(time(0)); 
+	for (i = 0; i< CANTIDAD; i++)  
+    { 
+	 	aleatorio=numeroAleatorio();
+        insertarDato(L,aleatorio);
+    } 
 
-
+	printf("\n****Lista generada****\n");
+	mostrarLista(L);
+	
+	printf("\n****Prueba Busqueda Lineal****\n");	
+	aleatorio=numeroAleatorio();
+	printf("\n-->Valor a consultar: %d \n", aleatorio);
+	resultado = findLinealSearch(L, aleatorio);
+	
+	if(resultado >= 0)
+		printf("\n-->El dato se encuentra en la posicion %d\n", resultado);	
+	else	
+		printf("\n-->El dato no esta en lista\n");
+	
+	int consulta;
+	printf("\n-->Consultar otro valor \n", aleatorio);
+	printf("\n<--Digite el valor para consultar: ");
+	scanf("%d", &consulta);
+	resultado = findLinealSearch(L, consulta);
+	
+	if(resultado >= 0)
+		printf("\n-->El dato se encuentra en la posicion %d\n", resultado);	
+	else	
+		printf("\n-->El dato no esta en lista\n");
+	
+	printf("\n****Prueba Busqueda Binaria****\n");
+	
+	resultado=obtenerTamano(L);
+	printf("\n-->Tamano de la lista: %d\n", resultado);
+	
+	int par=2;
+	for (i = 0; i< CANTIDAD; i++)  
+    { 
+        insertarDato(L2,par);
+        par=par+2;
+    } 
+    
+    printf("\n****Lista generada****\n");
+	mostrarLista(L2);
+    
+    printf("\n<--Digite el valor para consultar: ");
+	scanf("%d", &consulta);
+	resultado = findBinarySearch(L2, consulta, 0, resultado);
+	
+	if(resultado >= 0)
+		printf("\n-->El dato se encuentra en la posicion %d\n", resultado);	
+	else	
+		printf("\n-->El dato no esta en lista\n");
+			
+	liberarLista(L);
+	liberarLista(L2);
+}
 
